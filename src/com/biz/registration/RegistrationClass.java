0b4178495f271196b3b7cde67f505c3acc3d5b45
Makrand <makrandshingare@gmail.com>
2021-12-14 19:09:07 +0530

    Added : Added first name validation

--- src/com/biz/registration/RegistrationClass.java
+++ src/com/biz/registration/RegistrationClass.java
@@ -1,8 +1,23 @@
 package com.biz.registration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegistrationClass {
     public static void main(String[] args) {
-        System.out.println("Welcome in User Registration Problem");
+        Scanner sc= new Scanner(System.in);
+        String firstName=sc.next();
 
+        String regex="^[A-Z]{1}[a-z]{2,}$";
+
+        Pattern pattern=Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(firstName);
+        if(matcher.matches()){
+            System.out.println("Valid First name");
+        }
+        else {
+            System.out.println("Invalid First Name");
+        }
     }
 }
