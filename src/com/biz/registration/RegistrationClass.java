7b2e11944696e93c1d666b97812fd195101adfc2
Makrand <makrandshingare@gmail.com>
2021-12-14 18:56:18 +0530

    Welcome in User registeration

---
+++ src/com/biz/registration/RegistrationClass.java
@@ -0,0 +1,8 @@
+package com.biz.registration;
+
+public class RegistrationClass {
+    public static void main(String[] args) {
+        System.out.println("Welcome in User Registration Problem");
+
+    }
+}
